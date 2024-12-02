d05dc06b63484e2fb8fb6fe2b63603eeb10c908e
salmanhaider14 <salmanpatrick5@gmail.com>
2024-12-02 15:54:41 +0500

    Refactored SafetyCheck Par1 Day2 2024

--- AdventOfCode/2024/Day02.cs
+++ AdventOfCode/2024/Day02.cs
@@ -1,16 +1,13 @@
 using AdventOfCodeSupport;
-
 namespace AdventOfCode._2024;
-
 public class Day02: AdventBase
-{
+{ 
     protected override object InternalPart1()
     {
         return Input.Lines
             .Select(line => line.Trim().Split(" ").Select(long.Parse).ToList())
             .Count(SafetyCheck);
     }
-
     protected override object InternalPart2()
     {
         var safeReports = 0;
@@ -20,8 +17,7 @@ public class Day02: AdventBase
             var safe = SafetyCheck(numbers);
             if (safe)
                 safeReports++;
-                
-            else if (!safe)
+            else 
             {
                 for (int i = 0; i < numbers.Count; i++)
                 {
@@ -33,40 +29,27 @@ public class Day02: AdventBase
                         safeReports++;
                         break;
                     }
-
                 }
             }
-
         }
-
         return safeReports;
     }
 
     private bool SafetyCheck(List<long> numbers)
     {
         var increasing = numbers[0] < numbers[1];
-        var decreasing = !increasing;
         var safe = true;
 
         for (int i = 0; i < numbers.Count - 1; i++)
         {
             var current = numbers[i];
             var next = numbers[i + 1];
-            if (Math.Abs(current - next) is > 3 or < 1)
-            {
-                safe = false;
-                break;
-            }
-            if (increasing && next <= current)
+            
+            if ((increasing && next <= current) || (!increasing && next >= current) || (Math.Abs(current - next) is > 3 or < 1))
             {
                 safe = false;
                 break;
             } 
-            if (decreasing && next >= current)
-            {
-                safe = false;
-                break;
-            }
         }
         return safe;
     }
